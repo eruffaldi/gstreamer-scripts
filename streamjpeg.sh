85593e95a14e0b9a5b5a4b4b96164027d2fb6642
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-09-24 12:22:38 +0200

    added tee for cap

--- streamjpeg.sh
+++ streamjpeg.sh
@@ -22,8 +22,14 @@ TARGET=127.0.0.1
 PORT=9078
 
 if [ $1 == 'cap' ]; then
-	echo "Capture"
-	w="gst-launch-1.0 $CAP ! $ENCODER ! $PACKER ! udpsink host=$TARGET port=$PORT"
+	OUTPUT="udpsink host=$TARGET port=$PORT"
+	if [ $2 == 'show' ]; then
+		echo "Capture and Show"
+		w="gst-launch-1.0 $CAP ! tee name="local" ! queue ! autovideosink   local. ! $ENCODER ! $PACKER ! $OUTPUT"
+	else
+		echo "Capture"
+		w="gst-launch-1.0 $CAP ! $ENCODER ! $PACKER ! $OUTPUT"
+	fi
 	echo $w
 	$w
 elif [ $1 == 'play' ]; then
